4e01f19a4a42e1ed4edb90a4d189d82b60a3d500
Joao Victor <Carvalho.joao1@gmail.com>
2024-11-05 18:06:38 -0300

    configuração das nossas rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,47 @@
+import { CarDetail } from './pages/car'
+import { Dashboard } from './pages/dashboard'
+import { New } from './pages/dashboard/new'
+import { Home } from './pages/home'
+import { Login } from './pages/login'
+import { Register } from './pages/register'
 
-function App() {
 
-  return (
-    <>
-        <h1 className="bg-black text-white">APP</h1>
-    </>
-  )
-}
+import { Layout } from './components/layout'
+import { createBrowserRouter } from 'react-router-dom'
 
-export default App
+const router = createBrowserRouter(
+  [
+    {
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          element: <Home />
+        },
+        {
+          path: "/car/:id",
+          element: <CarDetail />
+        },
+        {
+          path: "/dashboard",
+          element: <Dashboard />,
+        },
+        {
+          path: "/dashboard/new",
+          element: <New />
+        }
+      ]
+    },
+    {
+      path: '/login',
+      element: <Login />,
+
+    },
+    {
+      path: '/register',
+      element: <Register />
+    }
+  ]
+)
+
+export {router};
