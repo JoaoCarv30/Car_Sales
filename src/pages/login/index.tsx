96b669135b46e4988516896f3039d4be06b81e4d
Joao Victor <Carvalho.joao1@gmail.com>
2024-11-05 20:11:48 -0300

    pagina register

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -5,7 +5,7 @@ import { Input } from '../../components/input';
 
 //VALIDAÇÃO DE FORMULÁRIO
 import { useForm } from 'react-hook-form';
-import {z} from 'zod';
+import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 
@@ -18,63 +18,65 @@ type FormData = z.infer<typeof schema>;
 
 export function Login() {
 
-  const {register, handleSubmit, formState:{errors}} = useForm<FormData>(
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>(
     {
       resolver: zodResolver(schema),
       mode: "onChange"
     }
   );
 
-  function onsubmit(data: FormData){
+  function onsubmit(data: FormData) {
     console.log(data);
   }
 
 
-    return (
-     <Container>
+  return (
+    <Container>
       <div className='w-full min-h-screen flex items-center justify-center flex-col gap-4'>
         <Link to='/' className='mb-6 max-w-sm w-full'>
-          <img 
-          src={logoImg}
-           alt='Logo'
-           className='w-full'
-           />
+          <img
+            src={logoImg}
+            alt='Logo'
+            className='w-full'
+          />
         </Link>
 
         <form
-        onSubmit={handleSubmit(onsubmit)}
-        className='bg-white max-w-xl w-full rounded-lg'
+          onSubmit={handleSubmit(onsubmit)}
+          className='bg-white max-w-xl w-full rounded-lg p-4'
         >
 
           <div className='mb-3'>
             <Input
-            type='email'
-            placeholder='Digite seu Email'
-            name='email'
-            error = {errors.email?.message}
-            register={register}
+              type='email'
+              placeholder='Digite seu Email'
+              name='email'
+              error={errors.email?.message}
+              register={register}
             />
           </div>
-          
-          
+
+
           <div className='mb-3'>
             <Input
-            type='password'
-            placeholder='Digite sua senha'
-            name='password'
-            error = {errors.password?.message}
-            register={register}
+              type='password'
+              placeholder='Digite sua senha'
+              name='password'
+              error={errors.password?.message}
+              register={register}
             />
           </div>
-          
-          
-        <button
-        type='submit'
-        >Acessar</button>
+
+
+          <button
+            className='bg-zinc-900 w-full rounded-md h-11 text-white h-10 font-medium'
+            type='submit'
+          >Acessar</button>
 
         </form>
+        <Link to='/register' className='text-zinc-900 hover:underline hover:cursor-pointer'>Ainda não possui uma conta ? Cadastre-se</Link>
 
       </div>
-     </Container>
-    )
-  }
+    </Container>
+  )
+}
--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,7 +1,92 @@
+import { Link } from 'react-router-dom';
+import logoImg from '../../assets/logo.svg';
+import { Container } from '../../components/container';
+import { Input } from '../../components/input';
+
+//VALIDAÇÃO DE FORMULÁRIO
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+
+const schema = z.object({
+  name: z.string().nonempty("Campo nome é obrigatório"),
+  email: z.string().email("Insira um email válido").nonempty("Campo obrigatório"),
+  password: z.string().nonempty("Campo obrigatório").min(6, "A senha deve ter no mínimo 6 caracteres"),
+});
+
+type FormData = z.infer<typeof schema>;
+
 export function Register() {
-    return (
-      <div>
-        <h1>Pagina Cadastro</h1>
-      </div>
-    )
+
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>(
+    {
+      resolver: zodResolver(schema),
+      mode: "onChange"
+    }
+  );
+
+  function onsubmit(data: FormData) {
+    console.log(data);
   }
+
+
+  return (
+    <Container>
+      <div className='w-full min-h-screen flex items-center justify-center flex-col gap-4'>
+        <Link to='/' className='mb-6 max-w-sm w-full'>
+          <img
+            src={logoImg}
+            alt='Logo'
+            className='w-full'
+          />
+        </Link>
+
+        <form
+          onSubmit={handleSubmit(onsubmit)}
+          className='bg-white max-w-xl w-full rounded-lg p-4'
+        >
+          <div className='mb-3'>
+            <Input
+              type='text'
+              placeholder='Digite seu nome completo'
+              name='name'
+              error={errors.name?.message}
+              register={register}
+            />
+          </div>
+
+          <div className='mb-3'>
+            <Input
+              type='email'
+              placeholder='Digite seu Email'
+              name='email'
+              error={errors.email?.message}
+              register={register}
+            />
+          </div>
+
+
+          <div className='mb-3'>
+            <Input
+              type='password'
+              placeholder='Digite sua senha'
+              name='password'
+              error={errors.password?.message}
+              register={register}
+            />
+          </div>
+
+
+          <button
+            className='bg-zinc-900 w-full rounded-md h-11 text-white h-10 font-medium'
+            type='submit'
+          >Acessar</button>
+
+        </form>
+        <Link to='/login' className='text-zinc-900'>Já tem uma conta? Acesse</Link>
+
+      </div>
+    </Container>
+  )
+}
