bb5c0242928d6293650bc4bcbe97d8f009836623
Joao Victor <Carvalho.joao1@gmail.com>
2024-11-06 13:45:34 -0300

    Proteção das rotas

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { Register } from './pages/register'
 
 import { Layout } from './components/layout'
 import { createBrowserRouter } from 'react-router-dom'
+import { Private } from './routes/Private'
 
 const router = createBrowserRouter(
   [
@@ -24,11 +25,11 @@ const router = createBrowserRouter(
         },
         {
           path: "/dashboard",
-          element: <Dashboard />,
+          element: <Private><Dashboard/></Private>,
         },
         {
           path: "/dashboard/new",
-          element: <New />
+          element:<Private> <New /> </Private>,
         }
       ]
     },
